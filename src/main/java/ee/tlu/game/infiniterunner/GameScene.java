57712e5688b02e3fcc3a3e77cc10c05d154acd85
RainBowBl00d <Innarraudsepp@gmail.com>
2026-04-27 18:27:17 +0300

    Implemented basic visuals

--- src/main/java/ee/tlu/game/infiniterunner/GameScene.java
+++ src/main/java/ee/tlu/game/infiniterunner/GameScene.java
@@ -1,16 +1,83 @@
 package ee.tlu.game.infiniterunner;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.animation.AnimationTimer;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
-public class GameScene extends Application {
+import java.util.ArrayList;
+import java.util.List;
 
-	public static void main(String[] args) {
-		launch(args);
+public class GameScene {
+	private Scene scene;
+	private Pane manguväli;
+	private AnimationTimer timer;
+
+	private Rectangle mangija;
+	private List<Rectangle> takistused = new ArrayList<>();
+
+	public GameScene() {
+		manguväli = new Pane();
+		scene = new Scene(manguväli, 800, 400);
+
+		mangija = new Rectangle(50, 50, Color.RED);
+		mangija.setX(100);
+		mangija.setY(300);
+		manguväli.getChildren().add(mangija);
+
+		scene.setOnKeyPressed(event -> {
+			switch (event.getCode()) {
+				case SPACE:
+					hyppa();
+					break;
+			}
+		});
+
+		alustaMangutsyklit();
 	}
 
-	@Override
-	public void start(Stage primaryStage) {
+	private void alustaMangutsyklit() {
+		timer = new AnimationTimer() {
+			@Override
+			public void handle(long now) {
+				uuendaMangu();
+			}
+		};
+		timer.start();
+	}
 
+	private void uuendaMangu() {
+		for (Rectangle takistus : takistused) {
+			takistus.setX(takistus.getX() - 5);
+		}
+
+		if (Math.random() < 0.01) {
+			looTakistus();
+		}
+
+		takistused.removeIf(t -> {
+			if (t.getX() < -50) {
+				manguväli.getChildren().remove(t);
+				return true;
+			}
+			return false;
+		});
+	}
+
+	private void looTakistus() {
+		Rectangle t = new Rectangle(30, 30, Color.BLUE);
+		t.setX(850);
+		t.setY(320);
+		takistused.add(t);
+		manguväli.getChildren().add(t);
+	}
+
+	private void hyppa() {
+		System.out.println("Hüpe!");
+	}
+
+	public Scene getScene() {
+		return scene;
 	}
 }
--- src/main/java/ee/tlu/game/infiniterunner/Main.java
+++ src/main/java/ee/tlu/game/infiniterunner/Main.java
@@ -1,16 +1,40 @@
 package ee.tlu.game.infiniterunner;
 
+import ee.tlu.game.infiniterunner.GameScene;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	public static void main(String[] args) {
-		launch(args);
-	}
-
 	@Override
 	public void start(Stage primaryStage) {
+		StackPane root = new StackPane();
+		Text tervitus = new Text("Tere tulemast Infinite Runner mängu!\n" +
+				"Vajuta hiirega siia, et alustada.\n\n" +
+				"Juhtimine: Space = Hüpe");
+		root.getChildren().add(tervitus);
 
+		Scene menuuStseen = new Scene(root, 800, 400);
+
+		root.setOnMouseClicked(event -> {
+			GameScene mang = new GameScene();
+			primaryStage.setScene(mang.getScene());
+		});
+
+		primaryStage.setTitle("Infinite Runner Projekt");
+		primaryStage.setScene(menuuStseen);
+
+		primaryStage.widthProperty().addListener((obs, oldVal, newVal) -> {
+			System.out.println("Akna laius on nüüd: " + newVal);
+		});
+
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
